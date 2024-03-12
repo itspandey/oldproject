d66ad6e7571bbf8a5bbd08f2ba699cd9767dc03e
Himanshu <itspandeyjii@gmail.com>
2024-03-12 12:47:54 +0530

    ok

--- app/src/main/java/com/codecaste/demo/m.java
+++ app/src/main/java/com/codecaste/demo/m.java
@@ -1,122 +1,105 @@
-package com.codecaste.demo;
+package com.codecaste.random;
 
 import android.os.Bundle;
-import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class m extends AppCompatActivity {
-    EditText etGitHubUser; // This will be a reference to our GitHub username input.
-    Button btnGetRepos;  // This is a reference to the "Get Repos" button.
-    TextView tvRepoList;  // This will reference our repo list text box.
-    RequestQueue requestQueue;  // This is our requests queue to process our HTTP requests.
+import java.util.ArrayList;
+import java.util.List;
 
-    String baseUrl = "https://api.github.com/users/";  // This is the API base URL (GitHub API)
-    String url;
+public class m extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private ProductAdapter productAdapter;
+    private List<ProductItem> productItemList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_m);
 
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        this.etGitHubUser = (EditText) findViewById(R.id.et_github_user);  // Link our github user text box.
-        this.btnGetRepos = (Button) findViewById(R.id.btn_get_repos);  // Link our clicky button.
-        this.tvRepoList = (TextView) findViewById(R.id.tv_repo_list);  // Link our repository list text output box.
-        this.tvRepoList.setMovementMethod(new ScrollingMovementMethod());  // This makes our text box scrollable, for those big GitHub contributors with lots of repos :)
+        productItemList = new ArrayList<>();
+        productAdapter = new ProductAdapter(productItemList);
+        recyclerView.setAdapter(productAdapter);
 
-        requestQueue = Volley.newRequestQueue(this);  // This setups up a new request queue which we will need to make HTTP requests.
-    }
-    private void clearRepoList() {
-        // This will clear the repo list (set it as a blank string).
-        this.tvRepoList.setText("");
+        fetchProductData();
     }
 
-    private void addToRepoList(String repoName, String lastUpdated) {
-        // This will add a new repo to our list.
-        // It combines the repoName and lastUpdated strings together.
-        // And then adds them followed by a new line (\n\n make two new lines).
-        String strRow = repoName + " / " + lastUpdated;
-        String currentText = tvRepoList.getText().toString();
-        this.tvRepoList.setText(currentText + "\n\n" + strRow);
-    }
+    private void fetchProductData() {
+        // Replace "your_product_api_url" with your actual product API endpoint
+        String apiUrl = "https://dummyjson.com/products/1";
 
-    private void setRepoListText(String str) {
-        // This is used for setting the text of our repo list box to a specific string.
-        // We will use this to write a "No repos found" message if the user doens't have any.
-        this.tvRepoList.setText(str);
-    }
+        RequestQueue queue = Volley.newRequestQueue(this);
 
-    private void getRepoList(String username) {
-        // First, we insert the username into the repo url.
-        // The repo url is defined in GitHubs API docs (https://developer.github.com/v3/repos/).
-        this.url = this.baseUrl + username + "/repos";
-
-        // Next, we create a new JsonArrayRequest. This will use Volley to make a HTTP request
-        // that expects a JSON Array Response.
-        // To fully understand this, I'd recommend readng the office docs: https://developer.android.com/training/volley/index.html
-        JsonArrayRequest arrReq = new JsonArrayRequest(Request.Method.GET, url,null,
-                new Response.Listener<JSONArray>() {
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.GET,
+                apiUrl,
+                null,
+                new Response.Listener<JSONObject>() {
                     @Override
-                    public void onResponse(JSONArray response) {
-                        // Check the length of our response (to see if the user has any repos)
-                        if (response.length() > 0) {
-                            // The user does have repos, so let's loop through them all.
-                            for (int i = 0; i < response.length(); i++) {
-                                try {
-                                    // For each repo, add a new line to our repo list.
-                                    JSONObject jsonObj = response.getJSONObject(i);
-                                    String repoName = jsonObj.get("name").toString();
-                                    String lastUpdated = jsonObj.get("updated_at").toString();
-                                    addToRepoList(repoName, lastUpdated);
-                                } catch (JSONException e) {
-                                    // If there is an error then output this to the logs.
-                                    Log.e("Volley", "Invalid JSON Object.");
-                                }
-
-                            }
-                        } else {
-                            // The user didn't have any repos.
-                            setRepoListText("No repos found.");
-                        }
-
+                    public void onResponse(JSONObject response) {
+                        parseProductApiResponse(response);
                     }
                 },
-
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        // If there a HTTP error then add a note to our repo list.
-                        setRepoListText("Error while calling REST API");
-                        Log.e("Volley", error.toString());
+                        Toast.makeText(m.this, "Error fetching product data", Toast.LENGTH_SHORT).show();
+                        Log.e("ProductData", "Error fetching product data: " + error.toString());
                     }
-                }
-        );
-        // Add the request we just defined to our request queue.
-        // The request queue will automatically handle the request as soon as it can.
-        requestQueue.add(arrReq);
+                });
+
+        queue.add(jsonObjectRequest);
     }
-    public void getReposClicked(View v) {
-        // Clear the repo list (so we have a fresh screen to add to)
-        clearRepoList();
-        // Call our getRepoList() function that is defined above and pass in the
-        // text which has been entered into the etGitHubUser text input field.
-        getRepoList(etGitHubUser.getText().toString());
+
+    private void parseProductApiResponse(JSONObject response) {
+        try {
+            int id = response.getInt("id");
+            String title = response.getString("title");
+            String description = response.getString("description");
+            double price = response.getDouble("price");
+            double discountPercentage = response.getDouble("discountPercentage");
+            double rating = response.getDouble("rating");
+            int stock = response.getInt("stock");
+            String brand = response.getString("brand");
+            String category = response.getString("category");
+            String thumbnail = response.getString("thumbnail");
+            List<String> images = parseImages(response.getJSONArray("images"));
+
+            ProductItem productItem = new ProductItem(id, title, description, price, discountPercentage,
+                    rating, stock, brand, category, thumbnail, images);
+
+            productItemList.add(productItem);
+            productAdapter.notifyDataSetChanged();
+        } catch (JSONException e) {
+            e.printStackTrace();
+            Toast.makeText(m.this, "Error parsing product JSON", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private List<String> parseImages(JSONArray imagesArray) throws JSONException {
+        List<String> imagesList = new ArrayList<>();
+        for (int i = 0; i < imagesArray.length(); i++) {
+            imagesList.add(imagesArray.getString(i));
+        }
+        return imagesList;
     }
 }
